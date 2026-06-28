8dd163177cdfe870a8e6c552a47c16db197dc6a8
vaishnaveedoray28 <vaishnaveedoray28@gmail.com>
2026-06-28 23:57:02 +0800

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
 Widget build(BuildContext context) {
   return MaterialApp( 
     title: 'UUM Lost & Found Rewards',
+    debugShowCheckedModeBanner: false,
     theme: ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
       useMaterial3: true,
